11459d7b4c0bf300378f91976b6c46edda868b34
JonathanC13 <jonhschan@hotmail.com>
2018-03-25 15:52:42 -0400

    M25

--- SketchProgram/WallMaze.ino
+++ SketchProgram/WallMaze.ino
@@ -27,67 +27,56 @@ void loop() {
 }
 
 /*
- * 000 forward
- * 101 forward
  * 
- * adjusting needed, i think
- * 
- * could do 90 degree turns if accurate, otherwise micro adjust turns on sensor conditions
- * 
- * 010 front blocked, always pick left
- * 
- * 
- * 011 left open
- * 110 right open
- * 
- * Two openings
- * 001
- * 100
- * 
- * 111 all blocked, 180, unless this indicates end
  */
 
 void run(){
+
+// LOW means wall
+// HIGH means no wall
+
+// middle sensor distance adjusted to be about half of left sensor
+// may just ignore right sensor.
+
   while(1){
-    if((digitalRead(left_sens) == WALL && digitalRead(middle_sens) == NOWALL && digitalRead(right_sens) == WALL) || (digitalRead(left_sens) == NOWALL && digitalRead(middle_sens) == NOWALL && digitalRead(right_sens) == NOWALL)){
-      moveForward();
-    }
-    else if(digitalRead(left_sens) == NOWALL && digitalRead(middle_sens) == WALL && digitalRead(right_sens) == NOWALL){
-      while(digitalRead(middle_sens) == WALL){
-        counterClockSpin();
-      }
-    }
-    // left opening
-    else if(digitalRead(left_sens) == NOWALL && digitalRead(middle_sens) == WALL && digitalRead(right_sens) == WALL){
-      while(digitalRead(middle_sens) == WALL){
-        counterClockSpin();
-      }
-    }
-    // right opening
-    else if(digitalRead(left_sens) == WALL && digitalRead(middle_sens) == WALL && digitalRead(right_sens) == NOWALL){
-      while(digitalRead(middle_sens) == WALL){
+
+   // high means it does not detect the wall, so move in an arch left until it finds it
+   while(digitalRead(left_sens) == HIGH || digitalRead(middle_sens) == HIGH){
+
+     archLeft();
+     
+     // If both detect a wall, then assume it is a corner.
+     if(digitalRead(left_sens) == LOW && digitalRead(middle_sens) == LOW){
+
+       while(digitalRead(middle_sens) == LOW){
+
+        turnRight();
+       }
+     }
+
+     if(digitalRead(left_sens) == LOW && digitalRead(middle_sens) == HIGH){
+
+      //detects wall on left, so spin away to setup for next archLeft
+      while(digitalRead(left_sens) == LOW){
+
         clockwiseSpin();
       }
-    }
-    // left and middle open
-    else if(digitalRead(left_sens) == NOWALL && digitalRead(middle_sens) == NOWALL && digitalRead(right_sens) == WALL){
-      while(){
+      
+     }
+
+     if(digitalRead(left_sens) == HIGH && digitalRead(middle_sens) == LOW){
+
+      // for now spin in place to put the wall on the left
+      while(digitalRead(left_sens) == HIGH){
+
+        archRight();
         
       }
-    }
-    // right and middle open
-    else if(digitalRead(left_sens) == NOWALL && digitalRead(middle_sens) == NOWALL && digitalRead(right_sens) == WALL){
-      while(){
-           
-      }
-    }
-    // all blocked,180 and go back, unless this indicates end. Only if partial turn, it will enter another state and continue the turn
-    else if(digitalRead(left_sens) == WALL && digitalRead(middle_sens) == WALL && digitalRead(right_sens) == WALL){
-      while(digitalRead(middle_sens) == WALL){
-        counterClockSpin();
-      }
-    }
+     }
+    
+   }
     
   }
+  
 }
 
